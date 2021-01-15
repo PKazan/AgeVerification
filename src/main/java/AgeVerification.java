674ddfdc9de156cf14f561f31f4cb70caafe920b
Paweł Kazanecki <kazan87@gmail.com>
2021-01-15 21:56:44 +0100

    added age verification method and tests for this method

--- src/main/java/AgeVerification.java
+++ src/main/java/AgeVerification.java
@@ -1,5 +1,13 @@
 public class AgeVerification {
     public static void main(String[] args) {
 
+        isGreaterOrEqual18(10);
+    }
+
+    public static boolean isGreaterOrEqual18(int age) {
+        if (age >= 18) {
+            return true;
+        }
+            return false;
     }
 }
