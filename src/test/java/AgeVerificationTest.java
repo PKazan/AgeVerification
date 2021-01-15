b6ffe04af6177ea6c21f45fa22b891ddd01639cd
Paweł Kazanecki <kazan87@gmail.com>
2021-01-15 21:58:50 +0100

    added tests for age verification method

--- src/test/java/AgeVerificationTest.java
+++ src/test/java/AgeVerificationTest.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AgeVerificationTest {
+
+    @Test
+    void isGreaterThan18() {
+        boolean result = AgeVerification.isGreaterOrEqual18(21);
+
+        Assertions.assertTrue(result);
+    }
+    @Test
+    void isEqualTo18() {
+        boolean result = AgeVerification.isGreaterOrEqual18(18);
+
+        Assertions.assertTrue(result);
+    }
+    @Test
+    void isLowerThan18() {
+        boolean result = AgeVerification.isGreaterOrEqual18(15);
+
+        Assertions.assertFalse(result);
+    }
+
 }
